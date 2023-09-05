84ef8ad2ab2973083e55908773a1ecf891da5f09
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-09-05 18:11:42 +0530

    assets collection

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#2c3333",
+        white: "#ffffdd",
+        gold: "wheat"
+      },
+      fontFamily: {
+        Poppins: ['Poppins', sans - serif],
+        "League Spartan": ['League Spartan', sans - serif],
+        Satisfy: ['Satisfy', cursive]
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow": "linear-gradient(90deg, rgba(131,58,180,0.9491968662464986) 0%, rgba(163,28,28,1) 50%, rgba(206,126,16,1) 100%);"
+      })
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    }
   },
   plugins: [],
 }
