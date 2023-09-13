801749b049c712674c213947caca21f679bd1ce5
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-09-13 13:05:43 +0530

    fix navbar

--- src/Pages/Home/Components/Navbar.jsx
+++ src/Pages/Home/Components/Navbar.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../../../hooks/useMediaQuery";
 import { Link } from "react-router-dom";
+import { FaAlignJustify, FaAsterisk } from "react-icons/fa";
+import { motion } from "framer-motion";
+
 
 const HeaderLink = ({ page, selectedPage, setSelectedPage }) => {
     const lowerCasePage = page.toLowerCase();
@@ -22,6 +25,8 @@ const HeaderLink = ({ page, selectedPage, setSelectedPage }) => {
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
 
     const isAboveMediumScreens = useMediaQuery("(min-width: 768px)");
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+
 
     return (
         <nav className={`z-40 w-full fixed top-0 py-5  mb-3`}>
@@ -37,30 +42,80 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                     isAboveMediumScreens ?
                         (
                             <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-                                <HeaderLink page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                                <HeaderLink page="about" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                                <HeaderLink page="Projects" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                                <Link to={"/blogs"} className=" hover:text-red-600 text-white transition duration-300
+                                <Link to={"https://dev.to/scorcism/"} target="_blank" className=" hover:text-red-600 text-white transition duration-300
         border-b-[1px] border-transparent hover:border-white cursor-pointer">Blogs</Link>
                                 <Link to={"/gallery"} className=" hover:text-red-600 text-white transition duration-300
         border-b-[1px] border-transparent hover:border-white cursor-pointer">Gallery</Link>
+                                <Link to={"/links"} className=" hover:text-red-600 text-white transition duration-300
+        border-b-[1px] border-transparent hover:border-white cursor-pointer">Links</Link>
                                 <HeaderLink page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
                             </div>
                         )
                         :
                         (
                             <button
-                                className="rounded-md bg-red p-2 border-2 
-                                py-2 px-5
-                                hover:text-gold
-                                transition duration-200
-                                "
+                                className="rounded-full bg-red p-2 "
+                                onClick={() => setIsMenuToggled(!isMenuToggled)}
                             >
-                                Blogs
+                                <FaAlignJustify />
                             </button>
                         )
                 }
 
+                {/* MOBILE MENU POPUP */}
+                {
+                    !isAboveMediumScreens && isMenuToggled && (
+                        <motion.div
+                            initial={{
+                                x: +60,
+                                opacity: 0
+                            }}
+                            whileInView={{
+                                opacity: 1,
+                                x: 0,
+                            }}
+                            transition={{
+                                duration: 0.2
+                            }}
+
+                            className="fixed right-0 bottom-0 h-full bg-black/90 w-[200px]">
+                            {/* CLOSE ICON */}
+                            <div className="flex justify-end p-12">
+                                <button onClick={() => {
+                                    setIsMenuToggled(!isMenuToggled)
+                                }}>
+                                    <FaAsterisk />
+                                </button>
+                            </div>
+
+                            {/* MENU ITEMS */}
+                            <motion.div
+                                initial={{
+                                    x: +60,
+                                    opacity: 0
+                                }}
+                                whileInView={{
+                                    opacity: 1,
+                                    x: 0,
+                                }}
+                                transition={{
+                                    duration: 0.3,
+                                    delay: 0.2
+                                }}
+
+                                className="flex flex-col gap-10 ml-[33%] text-2xl text-red-700 ">
+
+                                <Link to={"https://dev.to/scorcism/"} target="_blank" className=" hover:text-red-600 text-white transition duration-300
+        border-b-[1px] border-transparent hover:border-white cursor-pointer">Blogs</Link>
+                                <Link to={"/gallery"} className=" hover:text-red-600 text-white transition duration-300
+        border-b-[1px] border-transparent hover:border-white cursor-pointer">Gallery</Link>
+                                <Link to={"/links"} className=" hover:text-red-600 text-white transition duration-300
+        border-b-[1px] border-transparent hover:border-white cursor-pointer">Links</Link>
+                                <HeaderLink page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                            </motion.div>
+                        </motion.div>
+                    )
+                }
 
             </div>
 
